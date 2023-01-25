231286edf61609c4e278844cc98b3900c4bd20da
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-01-24 22:31:11 -0500

    update

--- src/main/java/pe/com/caichihua/backrest/demorestbackend/repository/ClienteRepository.java
+++ src/main/java/pe/com/caichihua/backrest/demorestbackend/repository/ClienteRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import pe.com.caichihua.backrest.demorestbackend.entity.ClienteEntity;
+import pe.com.caichihua.backrest.demorestbackend.entity.general.ClienteEntity;
 
 
 @Repository
