ca8a327b465fdc5ba6088802092db73aa065860d
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-01-26 01:56:21 -0500

    add update table productos

--- src/main/resources/scripts/scripts.sql
+++ src/main/resources/scripts/scripts.sql
@@ -16,3 +16,25 @@ VALUES('América Móvil Perú SAC','20467534026','(01) 315 - 0733');
 INSERT INTO tbl_cliente(RAZON_SOCIAL,RUC,TELEFONO)
 VALUES('Entel Móvil Perú SAC','20467534030','(01) 315 - 0737');
 
+
+
+
+CREATE TABLE tbl_producto
+   (ID_PRODUCTO int(11) primary key NOT NULL AUTO_INCREMENT,
+	NOMBRE VARCHAR(120),
+	PRECIO decimal(6,2),
+	STOCK decimal(4,0),
+	ESTADO CHAR(1) DEFAULT '1'
+);
+
+
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 1','2500','90','0');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 2','2500','16','0');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 3','500','14','0');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 4','2500','5','1');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 5','2500','16','1');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 6','2500','19','1');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 7','2500','19','1');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 8','2500','-5','1');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 9','2500','19','1');
+Insert into tbl_producto (NOMBRE,PRECIO,STOCK,ESTADO) values ('Producto 10','2500','19','1');
