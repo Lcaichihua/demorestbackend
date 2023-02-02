f9701e55cd6b1c33defe09344c56f6fc85e516c5
Larry Caichihua Vergara <lcaichihua@gmail.com>
2023-02-01 23:54:43 -0500

    enum EstadoType

--- src/main/java/pe/com/caichihua/backrest/demorestbackend/util/enums/EstadoType.java
+++ src/main/java/pe/com/caichihua/backrest/demorestbackend/util/enums/EstadoType.java
@@ -1,4 +1,18 @@
 package pe.com.caichihua.backrest.demorestbackend.util.enums;
 
 public enum EstadoType {
+    ACTIVO("ACTIVO",1),
+    INACTIVO("INACTIVO",0);
+
+    private String nombre;
+    private int clave;
+
+    private EstadoType (String nombre, int clave){
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
